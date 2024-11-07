186cbd9d1d243cf1781559b88db7e56510b5b94f
Suresh PK <141796572+sureshpk36@users.noreply.github.com>
2024-11-07 12:22:09 +0530

    Create sjf.c

---
+++ sjf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int bt[n], wt[n], tat[n];
+
+    printf("Enter burst times:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
+
+    // Sort burst times in ascending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (bt[i] > bt[j]) {
+                int temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+            }
+        }
+    }
+
+    // Calculate waiting times
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    // Calculate turnaround times
+    for (int i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];
+    }
+
+    // Display results and calculate averages
+    float total_wt = 0, total_tat = 0;
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    printf("\nAverage Waiting Time: %.2f\n", total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+
+    return 0;
+}
